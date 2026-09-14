8329b625943e56cc63e4a139dc7106f785578739
typicalninja <typicalsign@proton.me>
2026-09-14 17:38:18 +0530

    feat: add getStagedDiffs function

---
+++ src/lib/git.ts
@@ -0,0 +1,11 @@
+import { execa } from "execa";
+
+const execGit = (...args: string[]) => execa("git", args)
+
+/**
+ * Get the diff for all staged files in the current repository.
+ */
+export async function getStagedDiffs() {
+  const { stdout } = await execGit("diff", "--cached");
+  return stdout.toString();
+}
