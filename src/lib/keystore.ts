a74f296a2ba5f2a42c966af9f59bfb9c4bf2d4cb
typicalninja <typicalsign@proton.me>
2026-09-20 23:28:06 +0530

    feat: return account name from addToKeyChain

--- src/lib/keystore.ts
+++ src/lib/keystore.ts
@@ -9,10 +9,11 @@ const sha256 = (str: string) => crypto.createHash("sha256").update(str).digest("
  * Add the provided value to the device keychain.
  * Uses the first 5 letters of the hash of the value as keychain account.
  */
-async function addToKeyChain(value: string) {
+async function addToKeyChain(value: string): Promise<string> {
   // get the first 5 characters after hashing the value
   const accountName = sha256(value).substring(0, 5);
   await keytar.setPassword(SERVICE_NAME, accountName, value);
+  return accountName;
 }
 
 /**
