ed8583745c8faa6534e05364a281501e50a9e00e
typicalninja <typicalsign@proton.me>
2026-09-20 23:34:23 +0530

    feat: add keys add command to store google api key

---
+++ src/commands/keys/add.ts
@@ -0,0 +1,15 @@
+import { defineCommand } from "yalp-js";
+import { greenBright } from "yoctocolors";
+import keystore from "../../lib/keystore";
+
+export default defineCommand({
+  name: "add",
+  description: "Add a Google API key to be used for generating commit messages",
+  positionals: { key: { required: true } },
+  action: async ({ positionals }) => {
+    const { key } = positionals;
+    const savedName = await keystore.addToKeyChain(key);
+
+    console.log(`API key saved as ${greenBright(savedName)}`)
+  },
+});
