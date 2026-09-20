9c21936ef06241f5bc400c04ba9211704a03dbd3
typicalninja <typicalsign@proton.me>
2026-09-20 23:49:20 +0530

    feat: add key remove command and mask displayed keys

--- src/commands/key/index.ts
+++ src/commands/key/index.ts
@@ -1,17 +1,18 @@
 import { defineCommand } from "yalp-js";
 import { blueBright } from "yoctocolors";
-import add from "./add";
 import keystore from "../../lib/keystore";
+import add from "./add";
+import remove from "./remove";
 
 export default defineCommand({
   name: "key",
-  commands: [add],
+  commands: [add, remove],
   description: "View Google API keys used by aic",
   action: async () => {
     const availableKeys = await keystore.getKeysFromKeyChain();
-    console.log(`Keys available: [${availableKeys.length}]`)
+    console.log(`Keys available: [${availableKeys.length}]`);
     for (const { account, password } of availableKeys) {
-      console.log(`   ${blueBright(account)} -> ${password}`)
+      console.log(`   ${blueBright(account)} -> ${password.substring(0, 5)}...`);
     }
   },
 });
---
+++ src/commands/key/remove.ts
@@ -0,0 +1,19 @@
+import { defineCommand } from "yalp-js";
+import { blueBright, redBright } from "yoctocolors";
+import keystore from "../../lib/keystore";
+
+export default defineCommand({
+  name: "remove",
+  description: "Remove a Google API key from being used for generating commit messages",
+  positionals: { key: { required: true } },
+  action: async ({ positionals }) => {
+    const { key } = positionals;
+    const deleteStatus = await keystore.deleteKeyFromKeyChain(key);
+
+    if (deleteStatus) {
+      console.log(`deleted the key ${blueBright(key)}.`);
+    } else {
+      console.error(`failed to delete the key ${redBright(key)}.`);
+    }
+  },
+});
